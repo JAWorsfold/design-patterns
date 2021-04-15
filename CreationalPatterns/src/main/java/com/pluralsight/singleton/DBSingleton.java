e5bfdfd2413c8442b952df6a5e4448fefb5ff9f2
Joe Worsfold <joseph.worsfold@digital.homeoffice.gov.uk>
2021-04-15 13:04:58 +0100

    added example singleton for db connection

--- CreationalPatterns/src/main/java/com/pluralsight/singleton/DBSingleton.java
+++ CreationalPatterns/src/main/java/com/pluralsight/singleton/DBSingleton.java
@@ -1,5 +1,56 @@
 package com.pluralsight.singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBSingleton {
+
+    private static volatile DBSingleton instance = null;
+    private static volatile Connection conn = null;
+
+    private DBSingleton() {
+        try {
+            DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if(conn != null) {
+            throw new RuntimeException("Use getConnection() method to create");
+        }
+
+        if(instance != null) {
+            throw new RuntimeException("Use getInstance() method to create");
+        }
+
+    }
+
+    public static DBSingleton getInstance() {
+        if (instance == null) {
+            synchronized(DBSingleton.class) {
+                if (instance == null) {
+                    instance = new DBSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        if (conn == null) {
+            synchronized (DBSingleton.class) {
+                if (conn == null) {
+                    try {
+                        String dbURL = "jdbc:derby:memory:codejava/webdb;create=true";
+                        conn = DriverManager.getConnection(dbURL);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return conn;
+    }
 
 }
--- CreationalPatterns/src/main/java/com/pluralsight/singleton/DBSingletonDemo.java
+++ CreationalPatterns/src/main/java/com/pluralsight/singleton/DBSingletonDemo.java
@@ -1,5 +1,39 @@
 package com.pluralsight.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBSingletonDemo {
+
+    public static void main(String[] args) {
+        DBSingleton instance = DBSingleton.getInstance();
+
+        long timeStart;
+        long timeEnd;
+
+        // performance test
+        timeStart = System.currentTimeMillis();
+        Connection conn = instance.getConnection();
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Time to instantiate instance: " + (timeEnd - timeStart));
+
+        Statement sta;
+        String sqlQuery = "CREATE TABLE Address (ID INT, StreetName VARCHAR(20), CITY VARCHAR(20))";
+        try {
+            sta = conn.createStatement();
+            sta.executeUpdate(sqlQuery);
+            System.out.println("Table created");
+            sta.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // performance after already instantiated
+        timeStart = System.currentTimeMillis();
+        conn = instance.getConnection();
+        timeEnd = System.currentTimeMillis();
+        System.out.println("Time to get instance: " + (timeEnd - timeStart));
+    }
 
 }
