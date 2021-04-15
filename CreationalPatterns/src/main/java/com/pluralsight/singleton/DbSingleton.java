014e0853fd5d64f0003a085a7d73407c38651645
Joe Worsfold <joseph.worsfold@digital.homeoffice.gov.uk>
2021-04-15 12:14:36 +0100

    make singleton example thread safe

--- CreationalPatterns/src/main/java/com/pluralsight/singleton/DbSingleton.java
+++ CreationalPatterns/src/main/java/com/pluralsight/singleton/DbSingleton.java
@@ -2,13 +2,27 @@ package com.pluralsight.singleton;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = null;
+    private static volatile DbSingleton instance = null;
 
-    private DbSingleton() {}
+    private DbSingleton() {
+        // stopping use of reflection
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance() method to create");
+        }
+    }
 
+    // don't make entire method synchronized
     public static DbSingleton getInstance() {
+
         if (instance == null) {
-            instance = new DbSingleton();
+            // would use other object in real application for lock
+            synchronized(DbSingleton.class) {
+                // check again in case of another thread instantiating it
+                // while a current thread is waiting for lock
+                if (instance == null) {
+                    instance = new DbSingleton();
+                }
+            }
         }
         return instance;
     }
