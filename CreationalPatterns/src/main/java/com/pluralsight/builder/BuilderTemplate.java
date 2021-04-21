7ede6864cc5b65fbc14421945702c6f00fe72515
Joe Worsfold <joseph.worsfold@digital.homeoffice.gov.uk>
2021-04-21 11:40:24 +0100

    refactor

--- CreationalPatterns/src/main/java/com/pluralsight/builder/BuilderTemplate.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight.builder;
-
-// great for complex object construction with lots of parameters
-// used to make immutable objects
-// E.g. StringBuilder
-public class BuilderTemplate {
-
-}
--- CreationalPatterns/src/main/java/com/pluralsight/builder/BuilderTemplateDemo.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight.builder;
-
-public class BuilderTemplateDemo {
-
-    public static void main(String[] args) {
-        System.out.println("Demo");
-    }
-
-}
